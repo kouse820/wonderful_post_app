93ca015eca98bd5dc38c8f7ad4b819dc426d8e84
竹ヶ原光匡 <>
2025-08-15 01:49:55 +0900

    Task. 7-3 article作成→表示(index, show, new, create)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "articles#index" #追記
+  resources :articles #追記
   resources :sample_articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
