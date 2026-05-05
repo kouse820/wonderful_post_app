3f8838b7201618992e6a474cb6ed324794efb697
竹ヶ原光匡 <>
2026-05-05 17:08:31 +0900

    Task. 7-1 articleテーブルの実装

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+# POST /articles
+  def create
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "#{t('activerecord.models.article')}を作成しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "#{t('activerecord.models.article')}を編集しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+    edirect_to articles_url, notice: "#{t('activerecord.models.article')}を削除しました。"
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
